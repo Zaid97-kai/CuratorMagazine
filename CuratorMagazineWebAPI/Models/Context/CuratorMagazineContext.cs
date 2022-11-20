696d87658b45cbc3a48a50c3d4fcdaac4ce861a9
Заид Мингалиев <MingalievZZ@stud.kai.ru>
2022-11-20 15:08:25 +0300

    Add comments

--- CuratorMagazineWebAPI/Models/Context/CuratorMagazineContext.cs
+++ CuratorMagazineWebAPI/Models/Context/CuratorMagazineContext.cs
@@ -5,14 +5,42 @@ using DataProtectionKey = Microsoft.AspNetCore.DataProtection.EntityFrameworkCor
 
 namespace CuratorMagazineWebAPI.Models.Context
 {
+    /// <summary>
+    /// Class CuratorMagazineContext.
+    /// Implements the <see cref="DbContext" />
+    /// Implements the <see cref="CuratorMagazineWebAPI.Models.Context.ICuratorMagazineContext" />
+    /// Implements the <see cref="Microsoft.AspNetCore.DataProtection.EntityFrameworkCore.IDataProtectionKeyContext" />
+    /// </summary>
+    /// <seealso cref="DbContext" />
+    /// <seealso cref="CuratorMagazineWebAPI.Models.Context.ICuratorMagazineContext" />
+    /// <seealso cref="Microsoft.AspNetCore.DataProtection.EntityFrameworkCore.IDataProtectionKeyContext" />
     public class CuratorMagazineContext : DbContext, ICuratorMagazineContext, Microsoft.AspNetCore.DataProtection.EntityFrameworkCore.IDataProtectionKeyContext
     {
+        /// <summary>
+        /// Initializes a new instance of the <see cref="CuratorMagazineContext"/> class.
+        /// </summary>
+        /// <param name="options">The options.</param>
         public CuratorMagazineContext(DbContextOptions<CuratorMagazineContext> options)
             : base(options)
         {
             Database.EnsureCreated();
         }
 
+        /// <summary>
+        /// Override this method to further configure the model that was discovered by convention from the entity types
+        /// exposed in <see cref="T:Microsoft.EntityFrameworkCore.DbSet`1" /> properties on your derived context. The resulting model may be cached
+        /// and re-used for subsequent instances of your derived context.
+        /// </summary>
+        /// <param name="modelBuilder">The builder being used to construct the model for this context. Databases (and other extensions) typically
+        /// define extension methods on this object that allow you to configure aspects of the model that are specific
+        /// to a given database.</param>
+        /// <remarks><para>
+        /// If a model is explicitly set on the options for this context (via <see cref="M:Microsoft.EntityFrameworkCore.DbContextOptionsBuilder.UseModel(Microsoft.EntityFrameworkCore.Metadata.IModel)" />)
+        /// then this method will not be run.
+        /// </para>
+        /// <para>
+        /// See <see href="https://aka.ms/efcore-docs-modeling">Modeling entity types and relationships</see> for more information.
+        /// </para></remarks>
         protected override void OnModelCreating(ModelBuilder modelBuilder)
         {
             modelBuilder.ApplyConfiguration(new UserConfiguration());
@@ -22,14 +50,38 @@ namespace CuratorMagazineWebAPI.Models.Context
 
         #region Entities
 
+        /// <summary>
+        /// Gets or sets the users.
+        /// </summary>
+        /// <value>The users.</value>
         public DbSet<User> Users { get; set; }
+        /// <summary>
+        /// Gets or sets the divisions.
+        /// </summary>
+        /// <value>The divisions.</value>
         public DbSet<Division> Divisions { get; set; }
+        /// <summary>
+        /// Gets or sets the groups.
+        /// </summary>
+        /// <value>The groups.</value>
         public DbSet<Group> Groups { get; set; }
+        /// <summary>
+        /// Gets or sets the parents.
+        /// </summary>
+        /// <value>The parents.</value>
         public DbSet<Parent> Parents { get; set; }
+        /// <summary>
+        /// Gets or sets the roles.
+        /// </summary>
+        /// <value>The roles.</value>
         public DbSet<Role> Roles { get; set; }
 
         #endregion
 
+        /// <summary>
+        /// A collection of <see cref="T:Microsoft.AspNetCore.DataProtection.EntityFrameworkCore.DataProtectionKey" />
+        /// </summary>
+        /// <value>The data protection keys.</value>
         public DbSet<DataProtectionKey> DataProtectionKeys { get; }
     }
 }
--- CuratorMagazineWebAPI/Models/Entities/Role.cs
+++ CuratorMagazineWebAPI/Models/Entities/Role.cs
@@ -1,9 +1,24 @@
 ﻿namespace CuratorMagazineWebAPI.Models.Entities
 {
+    /// <summary>
+    /// Class Role.
+    /// </summary>
     public class Role
     {
+        /// <summary>
+        /// Gets or sets the identifier.
+        /// </summary>
+        /// <value>The identifier.</value>
         public int Id { get; set; }
+        /// <summary>
+        /// Gets or sets the name.
+        /// </summary>
+        /// <value>The name.</value>
         public string Name { get; set; }
+        /// <summary>
+        /// Gets or sets the users.
+        /// </summary>
+        /// <value>The users.</value>
         public virtual List<User> Users { get; set; }
     }
 }
