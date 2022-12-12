4b7782d9bc19c89ff736cf0872a77c36176227a4
Заид Мингалиев <MingalievZZ@stud.kai.ru>
2022-12-12 21:37:09 +0300

    Add Swagger

--- CuratorMagazineWebAPI/Controllers/DivisionController.cs
+++ CuratorMagazineWebAPI/Controllers/DivisionController.cs
@@ -1,121 +1,119 @@
 ﻿using CuratorMagazineWebAPI.Models.Context;
 using CuratorMagazineWebAPI.Models.Entities;
-using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 
-namespace CuratorMagazineWebAPI.Controllers
+namespace CuratorMagazineWebAPI.Controllers;
+
+/// <summary>
+/// Class DivisionController.
+/// Implements the <see cref="Controller" />
+/// </summary>
+/// <seealso cref="Controller" />
+[ApiController]
+[Route("api/[controller]")]
+public class DivisionController : Controller
 {
     /// <summary>
-    /// Class DivisionController.
-    /// Implements the <see cref="Controller" />
+    /// The database
     /// </summary>
-    /// <seealso cref="Controller" />
-    [ApiController]
-    [Route("api/[controller]")]
-    public class DivisionController : Controller
+    private readonly CuratorMagazineContext _db;
+
+    /// <summary>
+    /// Initializes a new instance of the <see cref="DivisionController"/> class.
+    /// </summary>
+    /// <param name="context">The context.</param>
+    public DivisionController(CuratorMagazineContext context)
     {
-        /// <summary>
-        /// The database
-        /// </summary>
-        private readonly CuratorMagazineContext _db;
+        _db = context;
+        if (_db.Divisions.Any()) return;
+    }
 
-        /// <summary>
-        /// Initializes a new instance of the <see cref="DivisionController"/> class.
-        /// </summary>
-        /// <param name="context">The context.</param>
-        public DivisionController(CuratorMagazineContext context)
+    /// <summary>
+    /// Gets this instance.
+    /// </summary>
+    /// <returns>ActionResult&lt;IEnumerable&lt;Division&gt;&gt;.</returns>
+    [HttpGet]
+    public async Task<ActionResult<IEnumerable<Division>>> Get()
+    {
+        return await _db.Divisions.ToListAsync();
+    }
+
+    // GET: api/division/5
+    /// <summary>
+    /// Gets the specified identifier.
+    /// </summary>
+    /// <param name="id">The identifier.</param>
+    /// <returns>ActionResult&lt;Division&gt;.</returns>
+    [HttpGet("{id}")]
+    public async Task<ActionResult<Division>> Get(int id)
+    {
+        var division = await _db.Divisions.FirstOrDefaultAsync(x => x.Id == id);
+        if (division == null)
+            return NotFound();
+        return new ObjectResult(division);
+    }
+
+    // POST api/division
+    /// <summary>
+    /// Posts the specified division.
+    /// </summary>
+    /// <param name="division">The division.</param>
+    /// <returns>ActionResult&lt;Division&gt;.</returns>
+    [HttpPost]
+    public async Task<ActionResult<Division>> Post(Division division)
+    {
+        if (division == null)
         {
-            _db = context;
-            if (_db.Divisions.Any()) return;
+            return BadRequest();
         }
 
-        /// <summary>
-        /// Gets this instance.
-        /// </summary>
-        /// <returns>ActionResult&lt;IEnumerable&lt;Division&gt;&gt;.</returns>
-        [HttpGet]
-        public async Task<ActionResult<IEnumerable<Division>>> Get()
+        _db.Divisions.Add(division);
+        await _db.SaveChangesAsync();
+        return Ok(division);
+    }
+
+    // PUT api/division/
+    /// <summary>
+    /// Puts the specified division.
+    /// </summary>
+    /// <param name="division">The division.</param>
+    /// <returns>ActionResult&lt;Division&gt;.</returns>
+    [HttpPut]
+    public async Task<ActionResult<Division>> Put(Division division)
+    {
+        if (division == null)
         {
-            return await _db.Divisions.ToListAsync();
+            return BadRequest();
         }
 
-        // GET: api/division/5
-        /// <summary>
-        /// Gets the specified identifier.
-        /// </summary>
-        /// <param name="id">The identifier.</param>
-        /// <returns>ActionResult&lt;Division&gt;.</returns>
-        [HttpGet("{id}")]
-        public async Task<ActionResult<Division>> Get(int id)
+        if (!_db.Users.Any(x => x.Id == division.Id))
         {
-            var division = await _db.Divisions.FirstOrDefaultAsync(x => x.Id == id);
-            if (division == null)
-                return NotFound();
-            return new ObjectResult(division);
+            return NotFound();
         }
 
-        // POST api/division
-        /// <summary>
-        /// Posts the specified division.
-        /// </summary>
-        /// <param name="division">The division.</param>
-        /// <returns>ActionResult&lt;Division&gt;.</returns>
-        [HttpPost]
-        public async Task<ActionResult<Division>> Post(Division division)
-        {
-            if (division == null)
-            {
-                return BadRequest();
-            }
+        _db.Update(division);
+        await _db.SaveChangesAsync();
+        return Ok(division);
+    }
 
-            _db.Divisions.Add(division);
-            await _db.SaveChangesAsync();
-            return Ok(division);
+    // DELETE api/division/5
+    /// <summary>
+    /// Deletes the specified identifier.
+    /// </summary>
+    /// <param name="id">The identifier.</param>
+    /// <returns>ActionResult&lt;User&gt;.</returns>
+    [HttpDelete("{id}")]
+    public async Task<ActionResult<User>> Delete(int id)
+    {
+        var division = _db.Divisions.FirstOrDefault(x => x.Id == id);
+        if (division == null)
+        {
+            return NotFound();
         }
 
-        // PUT api/division/
-        /// <summary>
-        /// Puts the specified division.
-        /// </summary>
-        /// <param name="division">The division.</param>
-        /// <returns>ActionResult&lt;Division&gt;.</returns>
-        [HttpPut]
-        public async Task<ActionResult<Division>> Put(Division division)
-        {
-            if (division == null)
-            {
-                return BadRequest();
-            }
-
-            if (!_db.Users.Any(x => x.Id == division.Id))
-            {
-                return NotFound();
-            }
-
-            _db.Update(division);
-            await _db.SaveChangesAsync();
-            return Ok(division);
-        }
-
-        // DELETE api/division/5
-        /// <summary>
-        /// Deletes the specified identifier.
-        /// </summary>
-        /// <param name="id">The identifier.</param>
-        /// <returns>ActionResult&lt;User&gt;.</returns>
-        [HttpDelete("{id}")]
-        public async Task<ActionResult<User>> Delete(int id)
-        {
-            var division = _db.Divisions.FirstOrDefault(x => x.Id == id);
-            if (division == null)
-            {
-                return NotFound();
-            }
-
-            _db.Divisions.Remove(division);
-            await _db.SaveChangesAsync();
-            return Ok(division);
-        }
+        _db.Divisions.Remove(division);
+        await _db.SaveChangesAsync();
+        return Ok(division);
     }
 }
